6810929a09ce00d8dd3bdf05b33a7a1e15398809
okuda hiroko <okuda_hiroko@hotmail.com>
2020-05-31 09:39:31 +0000

    No.20

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <hi>Myプロフィール作成画面</hi>
-    </body>
-</html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'プロフィール作成画面'を埋め込む --}}
+@section('title','プロフィール作成画面')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+     <div class="container">
+         <div class="row">
+             <div class="col-md-8 mx-auto">
+                 <h2>My プロフィール</h2>
+             </div>
+         </div>
+     </div>
+@endsection     

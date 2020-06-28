90ec02a27e52b31f6696ee7f844923b335a58ac8
okuda hiroko <okuda_hiroko@hotmail.com>
2020-06-28 13:02:08 +0000

    kadai

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -33,6 +33,12 @@
                         </div>
                     </div>
                     <div class="form-group row">
+                        <label class="col-md-2" for="body">本文2</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
                         <label class="col-md-2" for="title">画像</label>
                         <div class="col-md-10">
                             <input type="file" class="form-control-file" name="image">
